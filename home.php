d22962d6e6bcef23e69a19d0afac3f891a62e488
Helder Malunga <heldermalunga@gmail.com>
2023-07-02 09:07:10 +0200

    versao 1.1

--- home.php
+++ home.php
@@ -6,12 +6,21 @@ require_once 'head.php';
 if(!isset($_SESSION['logado'])):
 header('Location:login.php');
 endif;
+
+$id=$_SESSION['id_session'];
+$sql="SELECT * FROM users WHERE id='$id'";
+$resultado=mysqli_query($conexao,$sql);
+$dados=mysqli_fetch_array($resultado);
+
+
+
+
 ?>
 <nav>
 
 	<ul>
 		<li>Logotipo do site</li>
-	    <li><p><?php echo $_SESSION['id_session'];?></p></li>
+	    <li><p><?php echo $dados['user'];?></p></li>
 	</ul>
 	<a href="processar_logout.php">Logout</a>
 </nav>
